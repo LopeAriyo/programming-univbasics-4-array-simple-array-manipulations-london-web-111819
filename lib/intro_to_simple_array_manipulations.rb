89ead1e9a49c0053da56d9a774390cea1699c3ca
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-01 10:31:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,59 @@
+def using_push(array,string)
+
+  array.push(string)
+
+end
+
+def using_unshift(array,string)
+
+  array.unshift(string)
+
+end
+
+def using_pop(array)
+
+  array.pop
+
+end
+
+def pop_with_args (array)
+
+  array.pop(2)
+
+end
+
+def using_shift (array)
+
+  array.shift
+
+end
+
+def shift_with_args (array)
+
+  array.shift(2)
+
+end
+
+def using_concat (array_one, array_two)
+
+  #array.shift
+
+end
+
+def using_insert (array, element)
+
+  #array.shift
+
+end
+
+def using_uniq (array)
+
+  #array.shift
+
+end
+
+def using_flatten (array)
+
+  #array.shift
+
+end
